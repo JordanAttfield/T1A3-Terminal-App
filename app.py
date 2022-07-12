2bb50f12a5f16d8c48c5024786c49829990197b0
JordanAttfield <jordan.attfield@outlook.com>
2022-07-12 15:48:07 +1000

    Adding shopping cart functionality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from questions import Question
 import random
 from seed import book_catalogue
 from os import system
+import addons
 
 
 def welcome():
@@ -14,10 +15,20 @@ def welcome():
     option = input("Please select your option (1, 2 or 3): ")
     return option
 
+book_questions = [
+    "What kind of movie is your favourite?\n(a)  Romance - The Notebook is a favourite!\n(b) I honestly couldn't pick - I love a broad range of everything!\n(c) Suspenseful movies with creepy twists\n\n",
+    "What's your ideal setting?\n(a) Somewhere warm and cozy where I can curl up with a book\n(b) Somewhere that's just asking to be explored!\n(c) Somewhere creepy and secluded\n\n",
+    "Who would you like to have dinner with?\n(a) Augustus Waters\n(b) Hermione Granger\n(c) Sherlock Holmes\n\n"
+]
+
+
 def popular_fiction():
     print(
         "Looks like you're in the mood for a Popular Fiction book!\n\nPlease see the book we have selected for you below:\n")
-    print(random.choice(book_catalogue["fiction"]))
+    chosen_book = random.choice(book_catalogue["fiction"])
+    print(chosen_book)
+    checkout(chosen_book, cart, cart_total)
+    return chosen_book
 
 
 def romance():
@@ -29,31 +40,35 @@ def thriller():
     print("Looks like you're in the mood for a Thriller book!\n\nPlease see the book we have selected for you below:\n")
     print(random.choice(book_catalogue["thrillers"]))
 
-
-book_questions = [
-    "What kind of movie is your favourite?\n(a)  Romance - The Notebook is a favourite!\n(b) I honestly couldn't pick - I love a broad range of everything!\n(c) Suspenseful movies with creepy twists\n\n",
-    "What's your ideal setting?\n(a) Somewhere warm and cozy where I can curl up with a book\n(b) Somewhere that's just asking to be explored!\n(c) Somewhere creepy and secluded\n\n",
-    "Who would you like to have dinner with?\n(a) Augustus Waters\n(b) Hermione Granger\n(c) Sherlock Holmes\n\n"
-]
-
 questions = [
     Question(book_questions[0]),
     Question(book_questions[1]),
     Question(book_questions[2])
 ]
 
+cart = ()
+cart_total = int()
+
+def checkout(chosen_book, cart, cart_total):
+    checkout_ans = input("Would you like to purchase? The price of all our new releases is $18 per book. (Please enter 'yes' or 'no'\n")
+    if checkout_ans == "yes":
+        cart.append(chosen_book)
+        cart_total += 18
+        print(cart)
+        print(f"Cart Total: ${cart_total}")
+    
+
+def addon_item():
+    answer = input("Would you like to add one of our goodies? (Type 'yes' or 'no'")
+    if answer == "yes":
+        print(addons.add_on_selection)
+   
 
 def add_book():
     print("Here is our selection of books: \n")
     for key, value in book_catalogue.items():
         print(key, *value, sep='\n')
-
-
-def add_on_items():
-    answer = input("Would you like to add on any goodies to your book purchase?\nPrint 'Yes' or 'No'")
-    if answer == "yes":
-           
-        
+    addon_item()
 
 
 def run_quiz(questions):
@@ -69,7 +84,6 @@ def run_quiz(questions):
     determine_genre(score)
     return score
 
-
 def determine_genre(score):
     if score <= 4:
         romance()
@@ -78,13 +92,11 @@ def determine_genre(score):
     elif (score >= 8) and (score <= 9):
         thriller()
 
-
 user_choice = ""
 
 while user_choice != 3:
     system('clear')
     user_choice = welcome()
-    system('clear')
     if user_choice == "1":
         run_quiz(questions)
     elif user_choice == "2":
